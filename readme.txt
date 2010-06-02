4aa8c29f8bca6071b036e5e65b2d5f0b06a92711
hkothari <hkothari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-02 17:01:34 +0000

    Added shortcode support.
    git-svn-id: https://plugins.svn.wordpress.org/simpleadplacement/tags/0.80@248212 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== simpleAdPlacement ===
+Contributors: hkothari 
+Donate link: http://www.hydronitrogen.com/
+Tags: advertising, ads, placement
+Requires at least: 2.0.2
+Tested up to: 2.9.2
+Stable tag: 0.80
+
+simpleAdPlacement aims to be just what its name says. It allows you to easily place your
+advertisements in specified locations of pages.
+Currently it supports placement of advertisements at the bottom of single posts, at the bottom
+of pages and using shortcodes.
+
+== Description ==
+
+simpleAdPlacement aims to be just what its name says. It allows you to easily place your
+advertisements in specified locations of pages.
+
+* Single Posts - This refers to the area after the post on the single post page. Usually before the tags and comments, etc.
+* Bottom of Page - This is the area at the very bottom of the page. After the footer.
+* Shortcodes - This allows you to use shortcodes to place ads wherever you want. Just insert [simpleAdPlacement]
+
+== ChangeLog ==
+
+= 0.80 =
+* Added shortcode support.
+
+= 0.75 =
+* Added the ability to add advertisements to the bottom of pages as well.
+
+= 0.5 =
+* This is the first released version, you can add things to the bottom of post pages.
---
+++ simpleAdPlacement.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: simpleAdPlacement 
+Plugin URI: http://www.hydronitrogen.com/projects/simpleadplacement/
+Description: A tool which allows the simple placement of ads after posts, or on the bottom of pages.
+Version: 0.80
+Author: Hamel Ajay Kothari
+Author URI: http://www.hydronitrogen.com/
+License: GPL2
+*/
+
+register_activation_hook(__FILE__, 'simpleAdInstall');
+register_deactivation_hook(__FILE__, 'simpleAdUninstall');
+
+function simpleAdInstall()
+{
+	add_option("simpleAd_postAdCode");
+	add_option("simpleAd_bottomAdCode");
+	add_option("simpleAd_shortAdCode");
+}
+
+function simpleAdUninstall()
+{
+	delete_option("simpleAd_postAdCode");
+	delete_option("simpleAd_bottomAdCode");
+	delete_option("simpleAd_shortAdCode");
+}
+
+if(is_admin())
+{
+	add_action("admin_menu", "simpleAd_adminMenu");
+
+	function simpleAd_adminMenu()
+	{
+		add_options_page("SimpleAdPlacement Options", "SimpleAdPlacement", "manage_options", "simpleAdOptions", "simpleAdOptionsHtml");
+	}
+
+	function simpleAdOptionsHtml()
+	{ ?>
+		<h2>SimpleAdPlacement Options</h2>
+		<form method=\"post\" action=\"options.php\">
+		<?php wp_nonce_field('update-options'); ?>
+		<table>
+			<tr>
+				<td>
+					<strong>Enter your ad code for the single posts here:</strong><br />
+					<textarea name="simpleAd_postAdCode" cols="50" rows="20"><?php echo get_option("simpleAd_postAdCode"); ?></textarea><br />
+				</td>
+				<td>
+					<strong>Enter you ad code for the bottom of the page here:</strong><br />
+					<textarea name="simpleAd_bottomAdCode" cols="50" rows="20"><?php echo get_option("simpleAd_bottomAdCode"); ?></textarea><br />
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<strong>Enter your ad code for the shortcode: (To use it, just type [simpleAdPlacement] in a post or widget)</strong><br />
+					<textarea name="simpleAd_shortAdCode" cols="50" rows="20"><?php echo get_option("simpleAd_shortAdCode"); ?></textarea><br />
+				</td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="simpleAd_postAdCode,simpleAd_bottomAdCode,simpleAd_shortAdCode" /><br />
+		<input type="submit" value="Save Changes" /><br />
+
+		If you feel this plugin was helpful, please consider giving it a good rating on wordpress.org and clicking the works button. Thanks!';
+
+	<?php }
+}
+
+add_filter('the_content', 'simpleAdPostAds');
+add_action('wp_footer', 'simpleAdBottomAds');
+
+function simpleAdPostAds($content)
+{
+	if(is_single())
+		return $content . "<br />\n" . get_option("simpleAd_postAdCode");
+	else
+		return $content;
+}
+
+function simpleAdBottomAds()
+{
+	echo "<div align=\"center\">" . get_option("simpleAd_bottomAdCode") .  "</div>";
+}
+
+add_shortcode("simpleAdPlacement", "simpleAdPlacementShortCode");
+
+function simpleAdPlacementShortcode()
+{
+	return get_option("simpleAd_shortAdCode");
+}
+
+?>
